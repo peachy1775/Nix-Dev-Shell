b4406f9e282b2590a593ec1b788acdcf054231cd
peachy1775 <204289133+peachy1775@users.noreply.github.com>
2025-05-28 15:44:40 -0500

    udpate scaleing issues on ultra monitors

--- flake.nix
+++ flake.nix
@@ -5,11 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }:
+  outputs = { self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -44,8 +40,8 @@
         ghidra
         binwalk
         fastjar
-        
-        #brute force hash's and login's
+
+        # Brute force/hash/login tools
         hydra
         medusa
         hashcat
@@ -67,47 +63,50 @@
         nasm
         pyright
         (python3.withPackages (python-pkgs: with python-pkgs; [
-         # Web exploitation
-         requests
-         flask
-         beautifulsoup4
-         urllib3
-         websockets
-         httpx
-         jwt
-        
-         # Reverse engineering / binary exploitation
-         pwntools
-         capstone
-         unicorn
-         keystone
-         angr
-         z3-solver
-        
-         # Crypto
-         pycryptodome
-         base58
-         gmpy2
-         sympy
-        
-         # General-purpose / scripting
-         numpy 
-         python
-         matplotlib
-         tqdm
-         ipython
-         black        
-         isort        
-         pytest         
-         mypy       
-         pipx      
-         ]))
+          # Web exploitation
+          requests
+          flask
+          beautifulsoup4
+          urllib3
+          websockets
+          httpx
+          jwt
+
+          # Reverse engineering / binary exploitation
+          pwntools
+          capstone
+          unicorn
+          keystone
+          angr
+          z3-solver
+
+          # Crypto
+          pycryptodome
+          base58
+          gmpy2
+          sympy
+
+          # General-purpose / scripting
+          numpy
+          python
+          matplotlib
+          tqdm
+          ipython
+          black
+          isort
+          pytest
+          mypy
+          pipx
+        ]))
       ];
 
       shellHook = ''
-
         echo "Life Is Peachy"
-          export PS1="\[\e[1;36m\]\A [GetHacked@\h] \[\e[38;5;151m\]\w\n\[\e[38;5;218m\]\\$\[\e[0m\] "
+        export PS1="\[\e[1;36m\]\A [GetHacked@\h] \[\e[38;5;151m\]\w\n\[\e[38;5;218m\]\\$\[\e[0m\] "
+
+        # Fix massive UI scaling
+        export QT_AUTO_SCREEN_SCALE_FACTOR=0
+        export QT_SCALE_FACTOR=1
       '';
     };
   };
